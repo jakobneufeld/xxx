3183f9b052b57e52a88a5bc354837625a2d81090
jakobneufeld <jakob.n.neufeld@gmail.com>
2019-11-21 18:39:44 +0100

    Update PapaTests.swift

--- Tests/PapaTests/PapaTests.swift
+++ Tests/PapaTests/PapaTests.swift
@@ -7,6 +7,7 @@ final class PapaTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        XCTFail()
         
     }
   
